1de02923e4ff277c9fb8fbddf212671b76017857
Chirag4452 <chiragc77715@gmail.com>
2025-10-31 12:41:55 +0530

    Update market ID and adjust candlestick retrieval period; enhance EMA calculation and logging

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { getEMA, getMACD, getMidPrices } from "./indicators";
 import { CandlestickApi, IsomorphicFetchHttpLibrary, ServerConfiguration } from "./lighter-sdk-ts/generated";
 const BASE_URL = "https://mainnet.zklighter.elliot.ai"
-const SOL_MARKET_ID = 2
+const SOL_MARKET_ID = 1
 
 async function getKlines(maeketId: number) {
     const klinesApi = new CandlestickApi({
@@ -11,13 +11,16 @@ async function getKlines(maeketId: number) {
         authMethods: {}
     });
 
-    const klines = await klinesApi.candlesticks(SOL_MARKET_ID, '5m', Date.now() - 1000 * 60 * 60 * 1, Date.now(), 50, false);
+    const klines = await klinesApi.candlesticks(SOL_MARKET_ID, '4h', Date.now() - 1000 * 60 * 60 * 96, Date.now(), 50, false);
     const midPrices = getMidPrices(klines.candlesticks);
-    console.log(midPrices.slice(-10));
+    console.log("MID Prices")
+    console.log(midPrices);
     const emas20s = getEMA(midPrices, 20);
+    console.log("EMA20s")
     console.log(emas20s.slice(-10));
+    console.log("MACDS")
     const macd = getMACD(midPrices);
-    console.log(macd.slice(-10));
+    console.log(macd);
 }
 
 getKlines(SOL_MARKET_ID);
--- indicators.ts
+++ indicators.ts
@@ -5,34 +5,37 @@ import type { Candlestick } from "./lighter-sdk-ts/generated";
 // [i,2,3,4]  \ ema
 export function getEMA(prices: number[], period: number): number[] {
     const multiplier = 2 / (period + 1);
-    const smaInterval = prices.length - period;
-    if (smaInterval < 1) {
+
+    if (prices.length < period) {
         throw new Error("Not enough prices provided");
     }
-
+    //calculate initial SMA
     let sma = 0;
-    for (let i = 0; i < smaInterval; i++) {
+    for (let i = 0; i < period; i++) {
         sma += (prices[i] ?? 0)
     }
-    sma /= smaInterval;
+    sma /= period;
 //sma until this code 
     const emas = [sma]
     for (let i = 0; i < period; i++) {
-        const ema = (emas[emas.length - 1] ?? 0) * (1 - multiplier) + (prices[smaInterval + i] ?? 0) * multiplier;
+        const ema = (emas[emas.length - 1] ?? 0) * (1 - multiplier) + (prices[i] ?? 0) * multiplier;
         emas.push(ema);
     }
     return emas;
 }
 
 export function getMidPrices(candlesticks: Candlestick[]){
-    return candlesticks.map(({open, close}) => Number(((open + close) / 2).toFixed(2)));
+    return candlesticks.map(({open, close}) => Number(((open + close) / 2).toFixed(3)));
 }
 
 export function getMACD(prices: number[]) {
     const ema26 = getEMA(prices, 26);// [].length = 26
-    const ema14 = getEMA(prices, 14);// [].length = 14
+    let ema12 = getEMA(prices, 12);// [].length = 12
 
-    const macd = ema26.slice(-14).map((_, index) => (ema26[index] ?? 0) - (ema14[index] ?? 0));
+    ema12 = ema12.slice(-ema26.length);
+
+    console.log(ema12.length, ema26.length);
+    const macd = ema12.map((_, index) => (ema12[index] ?? 0) - (ema26[index] ?? 0));
     return macd
 
 }
